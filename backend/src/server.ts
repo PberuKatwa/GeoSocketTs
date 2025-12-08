f9a2093ba56fc3d7b4cb6a4596910ec81b1672f7
pberu8531 <pberu8531@gmail.com>
2025-12-08 18:37:25 +0300

    Server start up v2:setup boostrap service

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,16 +6,23 @@ import logger from "./utils/logger";
 // import socketServer from "./socket/index.js";
 // createSocketIOServer(4000)
 
-async function bootsrapServer(){
+async function bootsrapServer():Promise<void>{
 
     try{
 
-        logger.info(`Starting up socket service.`)
+        logger.info(`Starting up socket server.`)
 
         const socketService = new SocketService()
+
+        await socketService.calculateRoute();
+        await socketService.startTracking();
+
+        logger.info(`Successfully started server`)
 
     }catch(error){
         logger.error(`Error in bootstrapping server start up`,error)
     }
 
 }
+
+bootsrapServer()
