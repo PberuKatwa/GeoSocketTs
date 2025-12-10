1048e25bb2fe5a72733eb2e88b73686d69eba7c7
pberu8531 <pberu8531@gmail.com>
2025-12-10 21:35:34 +0300

    use map composable v4:working

--- geoFrontend/src/services/map.service.ts
+++ geoFrontend/src/services/map.service.ts
@@ -151,17 +151,17 @@ class MapService{
 
     }
 
-    public chooseCoordinates(): Promise<[number, number]> {
+    public chooseCoordinates(): Promise<mapCoordinates> {
         if (!this.map) throw new Error(`The map was not initialized`);
 
         return new Promise((resolve) => {
             const handler = (event: maplibregl.MapMouseEvent ) => {
-            const coords: [number, number] = [
-                event.lngLat.lng,
-                event.lngLat.lat,
-            ];
-            this.map?.off("click", handler);  // remove listener after resolving
-            resolve(coords);
+                const coords: mapCoordinates = [
+                    event.lngLat.lng,
+                    event.lngLat.lat,
+                ];
+                this.map?.off("click", handler);  
+                resolve(coords);
             };
 
             if (!this.map) throw new Error(`The map was not initialized`);
