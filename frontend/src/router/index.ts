e465f641d103af7cf82b3b7cf9cf4445701b6ff5
pberu8531 <pberu8531@gmail.com>
2025-12-14 13:01:26 +0300

    CLeanup frontend v1:removed duplicate files

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 import map from '@/views/map.vue'
-import mapServe from '@/components/mapServe.vue'
-import mapServe2 from '@/components/mapServe2.vue'
 import mapServe3 from '@/components/mapServe3.vue'
 import LibreMap from '@/components/LibreMap.vue'
 
@@ -12,16 +10,6 @@ const routes:Array<RouteRecordRaw> =[
     path:'/map-test',
     name:'map-test',
     component:map
-  },
-  {
-    path:'/map-serve',
-    name:'map-test',
-    component:mapServe
-  },
-  {
-    path:'/map/:id',
-    name:'map-driver-id',
-    component:mapServe2
   },
   {
     path:'/map2/:id',
